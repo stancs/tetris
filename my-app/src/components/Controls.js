02a8c5b099aa19a1f69cbb31ef745105293c622a
Stan <stanley.s.lee@gmail.com>
2023-12-03 20:24:53 -0800

    Added initial controls

---
+++ my-app/src/components/Controls.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Controls = (props) => {
+  return (
+    <div className="controls">
+      {/* left */}
+      <button className="control-button" onClick={(e) => {}}>
+        Left
+      </button>
+
+      {/* right */}
+      <button className="control-button" onClick={(e) => {}}>
+        Right
+      </button>
+
+      {/* rotate */}
+      <button className="control-button" onClick={(e) => {}}>
+        Rotate
+      </button>
+
+      {/* down */}
+      <button className="control-button" onClick={(e) => {}}>
+        Down
+      </button>
+    </div>
+  );
+};
+
+export default Controls;
