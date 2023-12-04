7205eb3ff9ae820a7811051226013d189a81ef94
Stan <stanley.s.lee@gmail.com>
2023-12-04 02:13:16 -0800

    Add keyboard control

--- my-app/src/components/Controls.js
+++ my-app/src/components/Controls.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { moveDown, moveLeft, moveRight, rotate } from '../actions';
 
@@ -6,6 +6,40 @@ const Controls = props => {
   const dispatch = useDispatch();
   const isRunning = useSelector(state => state.game.isRunning);
   const gameOver = useSelector(state => state.game.gameOver);
+
+  useEffect(() => {
+    const handleKeyPress = event => {
+      if (!isRunning || gameOver) {
+        return;
+      }
+
+      switch (event.key) {
+        case 'ArrowLeft':
+          dispatch(moveLeft());
+          break;
+        case 'ArrowRight':
+          dispatch(moveRight());
+          break;
+        case 'ArrowUp':
+          dispatch(rotate());
+          break;
+        case 'ArrowDown':
+          dispatch(moveDown());
+          break;
+        default:
+          // Do nothing for other keys
+          break;
+      }
+    };
+
+    // Attach the event listener when the component mounts
+    document.addEventListener('keydown', handleKeyPress);
+
+    // Detach the event listener when the component unmounts
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [dispatch, isRunning, gameOver]);
 
   return (
     <div className={`controls`}>
