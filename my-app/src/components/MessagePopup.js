b6e545b4b0a87756990b63390fc112fd2df15eb9
Stan <stanley.s.lee@gmail.com>
2023-12-03 21:57:23 -0800

    Added initial mesage popup

---
+++ my-app/src/components/MessagePopup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Displays a message
+const MessagePopup = (props) => {
+  return (
+    <div className="message-popup">
+      <h1>Message Title</h1>
+      <p>Message info...</p>
+    </div>
+  );
+};
+
+export default MessagePopup;
